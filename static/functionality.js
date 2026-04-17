7c8602475510b1aa979cf6d4f8631f97919c2dd9
AbdulRahimFami <138885353+AbdulRahimFami@users.noreply.github.com>
2026-04-17 21:28:27 +0530

    Add files via upload

---
+++ static/functionality.js
@@ -0,0 +1,18 @@
+console.log("hello")
+
+document.getElementById("linked-in-btn").addEventListener('click',()=>{
+    window.open("https://www.linkedin.com/in/abdulrahim-fami-70b735282", "_blank");
+})
+
+
+document.getElementById("github-btn").addEventListener('click',()=>{
+    window.open("https://github.com/AbdulRahimFami", "_blank");
+})
+
+
+document.getElementById("instagram-btn").addEventListener('click',()=>{
+    window.open("https://www.instagram.com/abdul_rahim_fami/", "_blank");
+})
+
+
+
